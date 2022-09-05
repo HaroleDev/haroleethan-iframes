f770d92ab3bcf49a137912b0b0e52e1eb8fa9dbd
Harole <47141290+HaroleDev@users.noreply.github.com>
2022-09-05 22:37:47 +0700

    Update index.js

--- Web Player Snippet/index.js
+++ Web Player Snippet/index.js
@@ -866,28 +866,30 @@ function toggleFullScreen() {
         document.webkitIsFullScreen ||
         document.mozIsFullScreen ||
         document.msIsFullScreen) {
-        document.exitFullscreen
-            ? document.exitFullscreen()
-            : document.webkitExitFullscreen
-                ? document.webkitExitFullscreen()
-                : document.webkitCancelFullScreen
-                    ? document.webkitCancelFullScreen()
-                    : document.mozCancelFullScreen
-                        ? document.mozCancelFullScreen()
-                        : document.msRequestFullscreen
-                        && document.msExitFullscreen()
+        if (document.exitFullscreen) {
+            document.exitFullscreen()
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen()
+        } else if (document.webkitCancelFullScreen) {
+            document.webkitCancelFullScreen()
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen()
+        } else if (document.msRequestFullscreen) {
+            document.msExitFullscreen()
+        }
         fullscreenTooltip.setAttribute('data-tooltip-text', 'Full screen' + ' (f)')
     } else {
-        playfulVideoPlayer.requestFullscreen
-            ? playfulVideoPlayer.requestFullscreen()
-            : playfulVideoPlayer.webkitRequestFullScreen
-                ? playfulVideoPlayer.webkitRequestFullScreen()
-                : video.webkitEnterFullScreen
-                    ? video.webkitEnterFullScreen()
-                    : playfulVideoPlayer.mozRequestFullScreen
-                        ? playfulVideoPlayer.mozRequestFullScreen()
-                        : playfulVideoPlayer.msRequestFullScreen
-                        && playfulVideoPlayer.msRequestFullscreen()
+        if (playfulVideoPlayer.requestFullscreen) {
+            playfulVideoPlayer.requestFullscreen()
+        } else if (playfulVideoPlayer.webkitRequestFullScreen) {
+            playfulVideoPlayer.webkitRequestFullScreen()
+        } else if (video.webkitEnterFullScreen) {
+            video.webkitEnterFullScreen()
+        } else if (playfulVideoPlayer.mozRequestFullScreen) {
+            playfulVideoPlayer.mozRequestFullScreen()
+        } else if (playfulVideoPlayer.msRequestFullScreen) {
+            playfulVideoPlayer.msRequestFullscreen()
+        }
         fullscreenTooltip.setAttribute('data-tooltip-text', 'Exit full screen' + ' (f)')
     }
 }

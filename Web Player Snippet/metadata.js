8af4fd447c4b8376821ac94393789639bbb8d54e
Harole <47141290+HaroleDev@users.noreply.github.com>
2022-09-08 02:03:26 +0700

    Update metadata.js

--- Web Player Snippet/metadata.js
+++ Web Player Snippet/metadata.js
@@ -4,7 +4,7 @@ const videoMetadata = {
   video_poster:
         '//res.cloudinary.com/harole/video/upload/s--p6nXm3qO--/c_fill,h_720,q_auto:low,w_1280/v1658949913/Harole%27s%20Videos/Sample%20Videos/Feeding%20fish%20in%20Hue/IMG_1175_H264STREAM_vfelcj.jpg',
   HLS_src:
-        './media//hls%20streams/playlist.m3u8',
+        './media/hls%20streams/playlist.m3u8',
   HLS_codec: 'application/x-mpegURL',
   Fallback_src:
         '//link.storjshare.io/jwrbyl67eqxrubohnqibyqwsx75q/harole-video%2F2022%2FSample%20Videos%2FJuly%2022%202022%2FIMG_1175_FALLBACKSTREAM.mp4?wrap=0',
